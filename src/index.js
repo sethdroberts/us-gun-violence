78bff8009efb0cbe52132a10bbc749315fcce321
Seth Roberts <sethknock@gmail.com>
2020-09-30 18:05:55 -0400

    Add remaining layers and on-hover feature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,155 @@
+import { GoogleMapsOverlay } from '@deck.gl/google-maps';
+import { HexagonLayer } from '@deck.gl/aggregation-layers';
+import { ScatterplotLayer } from '@deck.gl/layers';
+import { HeatmapLayer } from '@deck.gl/aggregation-layers';
+
+const scatterplot = () => new ScatterplotLayer({
+    id: 'scatter',
+    data: './gundata.json',
+    opacity: 0.8,
+    filled: true,
+    radiusMinPixels: 2,
+    radiusMaxPixels: 5,
+    getPosition: d => [d.longitude, d.latitude],
+    getFillColor: d => d.n_killed > 0 ? [200, 0, 40, 150] : [255, 140, 0, 100],
+    pickable: true,
+    onHover: ({object, x, y}) => {
+        const el = document.getElementById('tooltip');
+        if (object) {
+          const { n_killed, n_injured, incident_id, notes} = object;
+          el.innerHTML = 
+          `<p>ID: ${incident_id}</br>
+            Killed: ${n_killed}</br>
+            Injured: ${n_injured}</br>
+            ${notes} </p>`
+          el.style.display = 'block';
+          el.style.opacity = 0.9;
+          el.style.left = x + 'px';
+          el.style.top = y + 'px';
+        } else {
+          el.style.opacity = 0.0;
+        }
+    },
+    onClick: ({object, x, y}) => {
+        window.open(`https://www.gunviolencearchive.org/incident/${object.incident_id}`)
+      },
+});
+
+const heatmap = () => new HeatmapLayer({
+    id: 'heat',
+    data: './gundata.json',
+    getPosition: d => [d.longitude, d.latitude],
+    getWeight: d => d.n_killed + (d.n_injured * 0.5),
+    radiusPixels: 60,
+});
+
+const hexagon  = () => new HexagonLayer({
+    id: 'hex',
+    data: './gundata.json',
+    getPosition: d => [d.longitude, d.latitude],
+    getElevationWeight: d => (d.n_killed * 2) + d.n_injured,
+    elevationScale: 100,
+    extruded: true,
+    radius: 1609,         
+    opacity: 0.6,        
+    coverage: 0.88,
+    lowerPercentile: 50
+});
+
 window.initMap = () => {
+    
     const map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.0, lng: -100.0},
         zoom: 5,
+        styles: [
+            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+            {
+              featureType: "administrative.locality",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "poi",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "geometry",
+              stylers: [{ color: "#263c3f" }],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#6b9a76" }],
+            },
+            {
+              featureType: "road",
+              elementType: "geometry",
+              stylers: [{ color: "#38414e" }],
+            },
+            {
+              featureType: "road",
+              elementType: "geometry.stroke",
+              stylers: [{ color: "#212a37" }],
+            },
+            {
+              featureType: "road",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#9ca5b3" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry",
+              stylers: [{ color: "#746855" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry.stroke",
+              stylers: [{ color: "#1f2835" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#f3d19c" }],
+            },
+            {
+              featureType: "transit",
+              elementType: "geometry",
+              stylers: [{ color: "#2f3948" }],
+            },
+            {
+              featureType: "transit.station",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "water",
+              elementType: "geometry",
+              stylers: [{ color: "#17263c" }],
+            },
+            {
+              featureType: "water",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#515c6d" }],
+            },
+            {
+              featureType: "water",
+              elementType: "labels.text.stroke",
+              stylers: [{ color: "#17263c" }],
+            },
+          ],
     });
-}
 
-// import { GoogleMapsOverlay } from '@deck.gl/google-maps';
-// import { HexagonLayer } from '@deck.gl/aggregation-layers';
-// import ScatterplotLayer 
+    const overlay = new GoogleMapsOverlay({
+        layers: [
+            scatterplot(),
+            heatmap(),
+            hexagon()
+        ],
+    });
+
+    overlay.setMap(map);
+}
